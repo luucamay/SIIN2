803a8a32215c2fb2264b28a050ad8d4fa11834e9
lucamay <lupemaydana@gmail.com>
2017-11-24 15:52:04 -0400

    added volley library, also an http handler

--- app/src/main/java/com/example/lupe/siin/MainActivity.java
+++ app/src/main/java/com/example/lupe/siin/MainActivity.java
@@ -12,6 +12,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -31,6 +39,32 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // Made by lupe
+
+        final TextView mTextView = (TextView) findViewById(R.id.mtextView);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://192.168.4.20:80/rest_ejecucion/API/ejecucion/ejecucion.php";
+        //String url ="http://138.197.154.220/my-jallupredix-project/index.php/estaciones";
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText("Salida: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("Eso no funcionó");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
     }
 
     @Override
@@ -85,4 +119,7 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    // From here to ahead is lupe's code
+
 }
